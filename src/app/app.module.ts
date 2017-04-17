6cc29fc0963e2c1b1fd69231f5a5a0070ba1f761
Buchenberger <gregory.buchenberger@charter.com>
2017-04-17 14:09:42 -0600

    ace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MdButtonModule, MdCheckboxModule, MdSidenavModule, MaterialModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AceEditorModule, AceEditorDirective } from 'ng2-ace-editor';
+
 
 import { AppComponent } from './app.component';
+import { AceComponent } from './ace/ace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AceComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule.forRoot(),
+    AceEditorModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AceComponent]
 })
 export class AppModule { }
